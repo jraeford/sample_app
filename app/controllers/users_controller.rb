60fd3c029ba78158e1146bd20f866e0f828a27aa
James Raeford <jraeford@hotmail.com>
2012-03-10 13:00:37 -0500

    Finished Signup Success of Lesson 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,19 @@ class UsersController < ApplicationController
     @title = "Sign up"
   end
 
+  def create
+    #raise params[:user].inspect
+    #raise params.inspect
+    @user = User.new(params[:user])
+    if @user.save
+      #redirect_to user_path(@user)
+      flash[:success] = "Welcome to the Sample App!"
+      #redirect_to @user
+      redirect_to @user, :flash => { :success => "Welcome to the Sample App!"}
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
+
 end
